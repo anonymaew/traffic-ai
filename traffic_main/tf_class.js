dccad7ad057308e46d5fbca66303db03be15997b
anonymaew <napatsrichan2001@gmail.com>
2019-07-14 17:12:46 +0700

    add collision detection

--- traffic_main/tf_class.js
+++ traffic_main/tf_class.js
@@ -7,22 +7,28 @@ This code is entirely written by Napat Srichan.
 
 /*
 TODO :
-lane.update()
 environment.update()
 */
 
 class car{
     constructor(position,velocity){
-        this.pos=position;
-        this.vel=velocity;
+        this.pos=position;        //in m
+        this.vel=velocity;        //in kmph
         this.collision=false;
         this.maxsp=0;
         this.length=0;
-        this.nextpos=0;
-        this.nextvel=0;
-        this.acc=acceleration;
+        this.acc=acceleration;    //in kmphpst
         this.id=parseInt(random(1000000));
         this.type="";
+    }
+    collisionCheck(objectPosition,objectVelocity){
+        var colvec=this.vel.copy().setMag((this.vel.mag()*this.vel.mag()-objectVelocity.mag()*objectVelocity.mag())/(2*this.acc*36)+this.length+2);
+        var obvec=objectPosition.copy().sub(this.pos);
+        //edge case
+        if(colvec.angleBetween(obvec)>=HALF_PI) return obvec.mag()<2;
+        if(colvec.copy().mult(-1).angleBetween(obvec.copy().sub(colvec))>=HALF_PI) return obvec.copy().sub(colvec).mag()<2;
+        //normal case
+        return obvec.copy().sub(colvec.copy().setMag(obvec.dot(colvec)/colvec.mag())).mag()<2;
     }
 }
 
@@ -78,11 +84,14 @@ class lane{
         this.carcustom=0;
     }
     update(time){
+        //if it is the time to spawn the car
         if(this.carlastadd<=time){
             var ci=new car(this.startpos,createVector(60,0).rotate(this.startdir));
             ci.
             this.carlastadd+=36000/this.flowrate;
         }
+        //if the car reach the end of the road
+        if(this.carlist.length>0) if(this.carlist[0].pos.dist(finishpos)<2) this.carlist.shift();
     }
 }
 
@@ -91,7 +100,7 @@ class light{
         this.pos=position;
         this.status="red";
     }
-    collisioncheck(position){
+    collisionCheck(position){
         return (this.pos.dist(position)<2 && status=="red");
     }
 }
@@ -105,10 +114,26 @@ class environment{
     update(speed){
         for(var sp=0;sp<speed;sp++){
             for(let light of this.lightlist){
-                //let the light thinking
+                //let the light thinking by nn?
             }
-            for(let lane of this.lanelist){
+            for(var ilane=0;ilane<this.lanelist;ilane++){
+                var lane=this.lanelist[ilane];
+                //add or delete cars
                 lane.update(this.steptime);
+                //check the collision for the car
+                for(let car of lane) car.collision=false;
+                //check red light
+                for(let car of lane) for(let light of this.lightlist) car.collision=light.checkCollision(car.pos);
+                //check the front car
+                for(var icar=lane.carlist.length-1;icar>0;icar--){
+                    lane.carlist[icar].collision=lane.carlist[icar].collisionCheck(lane.carlist[icar-1].pos,lane.carlist[icar-1].vel);
+                }
+                //update the car
+                for(let car of lane){
+                    if(car.collision) car.vel.sub(car.vel.copy().setMag(car.acc));
+                    else if(car.vel.mag()<car.maxsp) car.vel.add(car.vel.copy().setMag(car.acc));
+                    car.pos.add(car.vec);
+                }
             }
             this.steptime++;
         }

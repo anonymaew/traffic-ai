e31ffd5b7b3c826a94b113fdaa6c708b8094f1b1
anonymaew <napatsrichan2001@gmail.com>
2019-07-15 23:11:49 +0700

    add moving and graphics

--- traffic_main/sketch.js
+++ traffic_main/sketch.js
@@ -1,7 +1,30 @@
+var env1,setupData,speed=1;
+var timetable={
+	"normal":0,
+	"library":0
+};
+
+function preload(){
+	setupData=loadJSON("system_config.json");
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight, WEBGL);
+	frameRate(30);
+	strokeWeight(1);
+	env1=new environment();
+	env1.setting(setupData);
+	camera(0,0,500,0,0,0,0,1,0);
 }
 
 function draw() {
-
+	background(255);
+	env1.show();
+	env1.update(speed);
 }
+/*
+function keyPressed(){
+	if(key==='a' && speed!=1) speed=speed/2;
+	if(key==='s') speed=speed*2;
+}
+*/
--- traffic_main/tf_class.js
+++ traffic_main/tf_class.js
@@ -7,28 +7,35 @@ This code is entirely written by Napat Srichan.
 
 /*
 TODO :
-environment.update()
+solve environment.update() bug
 */
 
 class car{
     constructor(position,velocity){
         this.pos=position;        //in m
-        this.vel=velocity;        //in kmph
+        this.sp=velocity.mag();        //in kmph
+        this.head=velocity.normalize();
         this.collision=false;
         this.maxsp=0;
         this.length=0;
-        this.acc=acceleration;    //in kmphpst
+        this.acc=0;    //in kmphpst
         this.id=parseInt(random(1000000));
         this.type="";
     }
     collisionCheck(objectPosition,objectVelocity){
-        var colvec=this.vel.copy().setMag((this.vel.mag()*this.vel.mag()-objectVelocity.mag()*objectVelocity.mag())/(2*this.acc*36)+this.length+2);
+        var bool=false;
+        var colvec=this.head.copy().setMag((this.sp*this.sp-objectVelocity.mag()*objectVelocity.mag())/(2*this.acc)/36+this.length+4);
+        ellipse(this.pos.copy().add(this.head.copy().setMag(colvec.mag())).x,this.pos.copy().add(this.head.copy().setMag(colvec.mag())).y,2,2);
         var obvec=objectPosition.copy().sub(this.pos);
+        /*
         //edge case
-        if(colvec.angleBetween(obvec)>=HALF_PI) return obvec.mag()<2;
-        if(colvec.copy().mult(-1).angleBetween(obvec.copy().sub(colvec))>=HALF_PI) return obvec.copy().sub(colvec).mag()<2;
+        if(colvec.angleBetween(obvec)>=HALF_PI) bool=obvec.mag()<3;
+        else if(colvec.copy().mult(-1).angleBetween(obvec.copy().sub(colvec))>=HALF_PI) bool=obvec.copy().sub(colvec).mag()<3;
         //normal case
-        return obvec.copy().sub(colvec.copy().setMag(obvec.dot(colvec)/colvec.mag())).mag()<2;
+        else bool=obvec.copy().sub(colvec.copy().setMag(obvec.dot(colvec)/colvec.mag())).mag()<3;
+        */
+        bool=collideLineCircle(0,0,colvec.x,colvec.y,obvec.x,obvec.y,6);
+        return bool;
     }
 }
 
@@ -47,13 +54,8 @@ class spawnfunction{
         this.overallprob=0;
         this.carlist=[];
     }
-    addcar(name,carLength,maximumSpeed,acceleration,probability){
-        this.carlist.push({
-            type:name,
-            length:carLength,
-            maxsp:maximumSpeed,
-            acc:acceleration,
-        });
+    addcar(carObj,probability){
+        this.carlist.push(carObj);
         this.problist.push(probability);
         this.overallprob+=probability;
     }
@@ -61,13 +63,13 @@ class spawnfunction{
         var i=random(this.overallprob);
         for(var ii=0;ii<this.problist.length;ii++){
             if(i<this.problist[ii]){
-                var prob=problist[ii];
+                var prob=this.carlist[ii];
                 car.type=prob.type;
                 car.length=prob.length;
                 car.maxsp=prob.maxsp;
                 car.acc=prob.acc;
             }
-            i=i-problist[ii];
+            i=i-this.problist[ii];
         }
     } 
 }
@@ -83,15 +85,23 @@ class lane{
         this.carlastadd=0;
         this.carcustom=0;
     }
+    setting(s){
+        for(let tl of s.turnlist) this.turnlist.push(new turnpoint(createVector(tl.pos[0],tl.pos[1]),tl.steer,tl.tolane));
+        this.carcustom=new spawnfunction();
+        for(let cf of s.carcustom.carlist) this.carcustom.addcar(cf,0);
+        this.carcustom.problist=s.carcustom.problist;
+        this.carcustom.overallprob=s.carcustom.overallprob;
+    }
     update(time){
         //if it is the time to spawn the car
         if(this.carlastadd<=time){
-            var ci=new car(this.startpos,createVector(60,0).rotate(this.startdir));
-            ci.
+            var ci=new car(this.startpos.copy(),createVector(60,0).rotate(this.startdir));
+            this.carcustom.customize(ci);
+            this.carlist.push(ci);
             this.carlastadd+=36000/this.flowrate;
         }
         //if the car reach the end of the road
-        if(this.carlist.length>0) if(this.carlist[0].pos.dist(finishpos)<2) this.carlist.shift();
+        if(this.carlist.length>0) if(this.carlist[0].pos.dist(this.finishpos)<3) this.carlist.shift();
     }
 }
 
@@ -100,42 +110,81 @@ class light{
         this.pos=position;
         this.status="red";
     }
-    collisionCheck(position){
-        return (this.pos.dist(position)<2 && status=="red");
-    }
 }
 
 class environment{
     constructor(){
         this.lanelist=[];
         this.lightlist=[];
-        this.steptime=0;    // 1 step time equals to 0.1 second 
+        this.size=0;
+        this.steptime=0;    // 1 step time equals to 0.1 second
+    }
+    setting(s){
+        this.size=createVector(s.size[0],s.size[1]);
+        for(let lanei of s.lane){
+            var ilane=new lane(createVector(lanei.startpos[0],lanei.startpos[1]),createVector(lanei.finishpos[0],lanei.finishpos[1]),[],lanei.flowrate);
+            ilane.setting(lanei);
+            this.lanelist.push(ilane);
+        }
+        for(let ilight of s.light) this.lightlist.push(new light(createVector(ilight.pos[0],ilight.pos[1])));
     }
     update(speed){
         for(var sp=0;sp<speed;sp++){
             for(let light of this.lightlist){
                 //let the light thinking by nn?
             }
-            for(var ilane=0;ilane<this.lanelist;ilane++){
-                var lane=this.lanelist[ilane];
+            for(var ilane=0;ilane<this.lanelist.length;ilane++){
+                var lanei=this.lanelist[ilane];
                 //add or delete cars
-                lane.update(this.steptime);
+                lanei.update(this.steptime);
                 //check the collision for the car
-                for(let car of lane) car.collision=false;
+                for(let cari of lanei.carlist) cari.collision=false;
                 //check red light
-                for(let car of lane) for(let light of this.lightlist) car.collision=light.checkCollision(car.pos);
+                for(let cari of lanei.carlist) for(let ilight of this.lightlist) if(ilight.status="red") cari.collision=cari.collisionCheck(ilight.pos,createVector(0,0));
                 //check the front car
-                for(var icar=lane.carlist.length-1;icar>0;icar--){
-                    lane.carlist[icar].collision=lane.carlist[icar].collisionCheck(lane.carlist[icar-1].pos,lane.carlist[icar-1].vel);
+                for(var icar=lanei.carlist.length-1;icar>0;icar--){
+                    lanei.carlist[icar].collision=lanei.carlist[icar].collisionCheck(lanei.carlist[icar-1].pos,lanei.carlist[icar-1].head.copy().mult(lanei.carlist[icar-1].sp));
                 }
                 //update the car
-                for(let car of lane){
-                    if(car.collision) car.vel.sub(car.vel.copy().setMag(car.acc));
-                    else if(car.vel.mag()<car.maxsp) car.vel.add(car.vel.copy().setMag(car.acc));
-                    car.pos.add(car.vec);
+                for(let cari of lanei.carlist){
+                    if(cari.collision && cari.sp>0) cari.sp-=cari.acc;
+                    else if(cari.sp<cari.maxsp) cari.sp+=cari.acc;
+                    if(cari.sp<0) cari.sp=0;
+                    cari.pos.add(cari.head.copy().mult(cari.sp/36));
                 }
             }
             this.steptime++;
+        }
+    }
+    show(){
+        //draw the grass
+        fill(28,107,43);
+        rect(-this.size.x/2,-this.size.y/2,this.size.x,this.size.y);
+        //draw lanes
+        for(let ilane of this.lanelist){
+            push();
+            translate(ilane.startpos.x,ilane.startpos.y);
+            var lanevec=ilane.finishpos.copy().sub(ilane.startpos);
+            rotate(lanevec.heading());
+            fill(64);
+            rect(0,-2,lanevec.mag(),4);
+            pop();
+        }
+        //draw lights
+        for(let ilight of this.lightlist){
+            if(ilight.status=="red") fill(255,0,0);
+            if(ilight.status=="green") fill(0,255,0);
+            if(ilight.status=="yellow") fill(255,255,0);
+            ellipse(ilight.pos.x,ilight.pos.y,3,3);
+        }
+        //draw cars
+        for(let ilane of this.lanelist) for(let icar of ilane.carlist){
+            push();
+            translate(icar.pos.x,icar.pos.y);
+            rotate(icar.head.heading());
+            fill(128,0,0);
+            rect(0,-1.5,icar.length,3);
+            pop();
         }
     }
 }
